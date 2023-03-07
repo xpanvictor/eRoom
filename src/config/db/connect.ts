e3556591d313336f18bdc76f0e110bf83b33a090
xpanvictor <xpanvictor@gmail.com>
2023-03-07 20:03:40 +0100

    feat: lib types added for modules

--- src/config/db/connect.ts
+++ src/config/db/connect.ts
@@ -4,21 +4,22 @@ import config from "../../constants/config";
 import ProgrammingError from "../../error/technical/ProgrammingError";
 
 export default async function connectMongo() {
+  mongoose.connection.on("connected", () => {
+    // Plugins connect
+    mongoose.plugin(slug);
+    console.log(`${mongoose.connections[0].name} connected with mongoose`);
+  });
+  mongoose.connection?.on("disconnect", () => {
+    console.log(`${mongoose.connections[0].name} disconnected!`);
+  });
+
   if (!config.MONGO_CONN_URL)
     throw new ProgrammingError("Mongodb connection url not provided!");
   try {
-    const conn = await mongoose.connect(config.MONGO_CONN_URL, {
+    await mongoose.connect(config.MONGO_CONN_URL, {
       retryWrites: false,
       appName: "EClass database",
       autoIndex: false,
-    });
-    mongoose.connection.on("connected", () => {
-      // Plugins connect
-      mongoose.plugin(slug);
-      console.log(`${conn.connections[0].name} connected with mongoose`);
-    });
-    conn.connection?.on("disconnect", () => {
-      console.log(`${conn.connections[0].name} disconnected!`);
     });
   } catch (e) {
     console.log(e);
