359ac1b289446b2581d2cdd9a8b5941479ff2875
xpanvictor <xpanvictor@gmail.com>
2023-03-07 21:36:14 +0100

    feat: socket initiates and watches message

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv = require("dotenv");
 import http = require("http");
+import { Server } from "socket.io";
 import serverConfig from "./constants/config";
 import app = require("./App");
 import Exception = require("./error/RejectionException/unhandled");
@@ -8,16 +9,22 @@ import connectMongo from "./config/db/connect";
 dotenv.config();
 
 async function main() {
-  // todo: work on the socket attachment here
-  const server = http.createServer(app);
+  // HTTP server docked
+  const httpServer = http.createServer(app);
+  // Socket.io server docked
+  const io = new Server(httpServer, {});
+  io.on("connection", (socket) => {
+    console.log("Socket connection initialized");
+    socket.on("message", (msg) => console.log(msg));
+  });
   // handle runtime ignored errors
-  Exception.unhandledRejection(server);
+  Exception.unhandledRejection(httpServer);
   Exception.unhandledException();
   // connect the database here
   await connectMongo();
   // listener
   const { ENV, PORT } = serverConfig;
-  server.listen(PORT, () => {
+  httpServer.listen(PORT, () => {
     console.log(`${ENV.toUpperCase()} 💯server listening at localhost:${PORT}`);
   });
 }
