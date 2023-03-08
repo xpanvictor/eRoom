3b283c2910f9858990542a7b528ca0315493d334
xpanvictor <xpanvictor@gmail.com>
2023-03-08 08:51:16 +0100

    feat: watches and verifies message object

--- src/services/chatChannel/chatChannel.controller.ts
+++ src/services/chatChannel/chatChannel.controller.ts
@@ -1,14 +1,34 @@
+import Joi from "joi";
 import { Listener } from "../../lib/types/socket/events.type";
+import { ChatMessage } from "./chatChannel.type";
 
-const handleChat: Listener<any> = () => {
+const messageSchema = Joi.object<ChatMessage>({
+  timestamp: Joi.date().timestamp().required(),
+  sender: Joi.string().required(),
+  channel: Joi.string(),
+  message: Joi.string().required(),
+});
+
+const handleChat: Listener<Partial<ChatMessage>> = (payload, callBack) => {
   // this means message has entered
-  // validate and serialize the message
+  // validate and sanitize the message
+  const { error, value } = messageSchema.validate(payload);
+  if (error) {
+    return console.log("An error occurred", error);
+    // todo: proper socket error handling
+  }
+  console.log("Message received: ", value);
   // determine sender of the message
   // extract message data
   // create message object
   // extract channel data
   // temporary channel data store
+  // perform necessary action for event
+  if (typeof callBack === "function") {
+    callBack();
+  }
   // permanently push channel data to mongo store
+  return 1;
 };
 
 export default handleChat;
