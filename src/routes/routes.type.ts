e1228e5de08df7e368d8753b6afe053760214ee2
xpanvictor <xpanvictor@gmail.com>
2023-03-09 21:15:41 +0100

    feat: adds user.service class to user of request object

---
+++ src/routes/routes.type.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Response } from "express";
+import { ModifiedRequest } from "../controllers/base.types";
+
+export enum RouteMethods {
+  GET = "get",
+  POST = "post",
+  PUT = "put",
+  DELETE = "delete",
+}
+
+export interface DRouteConfig {
+  // method: the REST method used to call path
+  method: RouteMethods;
+  // path: the path string called by method
+  path: string;
+}
+
+export type ListenerType = (
+  req: ModifiedRequest,
+  res: Response,
+  next: NextFunction
+) => void;
